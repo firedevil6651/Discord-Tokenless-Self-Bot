7257be9acb0c2a927e1e9085f71939100b58715a
firedevil6651 <73210077+firedevil6651@users.noreply.github.com>
2021-05-30 19:05:11 -0700

    Fixed to work after Discord design update and added more features

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# Discord Token-less Self Bot
-  A Token-less Discord Self Bot that has no other purpose but to keep you online, more features will come as I update the program. It uses ChromeDriver and Python to run. 
+# Discord Tokenless Self Bot
+  A Tokenless Discord Self Bot that I made using Python 3 and ChromeDriver. More features will come as I update the program. The program is open-source so if you think something is suspicious, you can check it for yourself!
 
 # Usage
-  The first thing you need to have to run this is Python (https://www.python.org/downloads/). Once that is done, edit the config.json file and add your email and password, if you have 2fa, make sure you put in your code to the json file aswell. Once you have that done, run the bat file, and let the program do it's magic!
+  The first thing you need to have to run this is Python (https://www.python.org/downloads/). Once that is done, edit the config.json file and add your email and password, if you have 2FA, you will be asked for your code later. Once you have that done, run the bat file or the python file directly, and let the program run!
+
+# Features
+
+Current features include:
+
+- Interactive command prompt
+- Status switching
+
+Features I plan to add if possible:
+
+- Channel messaging
+- Call Answering
+- VC Joining
+- Friend Adding
+- Email and password changing
+- Custom status setting
+- Account switching
+- Hypesquad switching
+- Nitro plan switching
 
 # Legality
-  As this program doesn't use a Discord user token, I am not sure if this is against Discord TOS so please be warned, I am not responsible for anything that happens to you or your account(s).
+  As this program doesn't use a Discord user token, I am not sure if this is against Discord TOS so don't say you haven't been warned, I am not responsible for anything that happens to you or your account(s). I, the creator will not be held accountable for anything that happens from any usage of this tool/program.
--- src/main.py
+++ src/main.py
@@ -1,73 +1,198 @@
-import time
+import os
 import json
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException        
+from cmd import Cmd
+try:
+    from colorama import Fore, init
+except:
+    os.system('pip install colorama')
+try:
+    from selenium import webdriver
+    from selenium.common.exceptions import NoSuchElementException
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.common.exceptions import TimeoutException
+except:
+    os.system('pip install selenium')
+
+init(autoreset=True)
 
 json_file = open("../config.json")
 variables = json.load(json_file)
 json_file.close()
 email = variables["email"]
 password = variables["password"]
-t2fa = variables["t2fa"]
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+cls()
+
+def online():
+    driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div[2]/div[1]/div').click()
+    WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="status-picker-online"]/div'))).click()
+    print('[STATUS] Successfully changed status to online!')
+def idle():
+    driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div[2]/div[1]/div').click()
+    WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="status-picker-idle"]/div'))).click()
+    print('[STATUS] Successfully changed status to idle!')
+def dnd():
+    driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div[2]/div[1]/div').click()
+    WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="status-picker-dnd"]/div'))).click()
+    print('[STATUS] Successfully changed status to DND!')
+def offline():
+    driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div[2]/div[1]/div').click()
+    WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="status-picker-invisible"]/div'))).click()
+    print('[STATUS] Successfully changed status to offline!')
+
+banner = Fore.RED + '''
+\n\n
+------------------------------------------------\n
+ __     __   __   __   __   __              
+|  \ | /__` /  ` /  \ |__) |  \             
+|__/ | .__/ \__, \__/ |  \ |__/             
+                                            
+___  __        ___            ___  __   __  
+ |  /  \ |__/ |__  |\ | |    |__  /__` /__` 
+ |  \__/ |  \ |___ | \| |___ |___ .__/ .__/ 
+                                            
+ __   ___       ___  __   __  ___           
+/__` |__  |    |__  |__) /  \  |            
+.__/ |___ |___ |    |__) \__/  |            \n
+
+------------------------------------------------\n\n\n'''
+
+print(banner)
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
-driver.get('https://discord.com/app/')
-source = driver.page_source, 'lxml'
-email_box = driver.find_element_by_name('email')
-password_box = driver.find_element_by_name('password')
-submit_button = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]')
-email_box.send_keys(email)
-password_box.send_keys(password)
-submit_button.click()
+driver.get('https://discord.com/app')
+WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.NAME, 'email'))).send_keys(email)
+print('[LOGIN] Input email')
+driver.find_element_by_name('password').send_keys(password)
+print('[LOGIN] Input password')
+try:
+    driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/div/form/div/div/div[1]/div[2]/button[2]').click()
+except NoSuchElementException:
+    driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[2]/button[2]').click()
+print('[LOGIN] Clicked login button')
+try:
+    WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[1]/h5/span')))
+    print(Fore.RED + '[LOGIN] Incorrect email or password, please double check the "config.json file"!')
+    driver.quit()
+    exit()
+except:
+    pass
 
 try:
-    driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/div[2]/h5/span')
-except NoSuchElementException:
-    invalidpasemail = False
-invalidpasemail = True
+    WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div')))
+    nd = True
+except:
+    nd = False
 
-if invalidpasemail == False:
-    print("Invalid email or password! Please double check the config.json file to make sure you didn't make a typo!")
-else:
+if nd == True:
     try:
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/form/div/div[3]/div/h5/span')
-    except NoSuchElementException:
-        input2fa = False
-    input2fa = True
+        WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div/section/div/div[1]')))
+        captcha = True
+    except:
+        captcha = False
 
-    if input2fa == False:
-        time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/form/div/div[3]/div/div/input').send_keys(t2fa)
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/form/div/div[3]/button[1]').click()
+    if captcha == True:
+        input('[CAPTCHA] A captcha has appeared! Press enter once you are finished!\n')
+    elif captcha == False:
+        pass
 
+    try:
+        WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div/form/div/div[1]')))
+        twofa = True
+    except:
+        twofa = False
+
+    if twofa == True:
+        twofacode = input('[2FA] Please enter your 2FA code: ')
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/form/div/div[2]/div/div/input').send_keys(twofacode)
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div/form/div/div[2]/button[1]').click()
         try:
-            driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/form/div/div[3]/div/h5/span')
-        except NoSuchElementException:
-            invalid2fa = False
-        invalid2fa = True
+            WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div/form/div/div[2]/div/h5'))).is_displayed()
+            print(Fore.RED + '[2FA] Invalid 2FA code!')
+            driver.quit()
+            exit()
+        except:
+            pass
+    elif twofa == False:
+        pass
 
-        if invalid2fa == True:
-            print("2FA code is invalid! Please use valid backup or authenticator code!")
+elif nd == False:
+    try:
+        WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/section/div/div[1]')))
+        captcha = True
+    except:
+        captcha = False
+
+    if captcha == True:
+        input('[CAPTCHA] A captcha has appeared! You have 2 options you can take, make ChromeDriver non-headless and restart the program, or, if you already have ChromeDriver in non-headless, solve the captcha on screen and press enter.\n')
+    elif captcha == False:
+        pass
+
+    try:
+        WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/form/div/div[1]')))
+        twofa = True
+    except:
+        twofa = False
+
+    if twofa == True:
+        twofacode = input('[2FA] Please enter your 2FA code: ')
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/form/div/div[2]/div/div/input').send_keys(twofacode)
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/form/div/div[2]/button[1]').click()
+        try:
+            WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div/form/div/div[2]/div/h5'))).is_displayed()
+            print(Fore.RED + '[2FA] Invalid 2FA code!')
+            driver.quit()
+            exit()
+        except:
+            pass
+    elif twofa == False:
+        pass
+
+print('[LOGIN] Please wait as you are logged in...')
+
+try:
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div[2]/div[3]/button[3]')))
+    print(Fore.GREEN + '[LOGIN] Successfully logged in!')
+except TimeoutException:
+    print(Fore.RED + '[LOGIN] An unexpected error occurred!')
+    driver.quit()
+    exit()
+
+print('[CMD] To view a list of all current commands in the interactive command prompt, type "help"!')
+print('\n\n')
+
+class InteractiveCMD(Cmd):
+
+    def do_exit(self, inp):
+        print('[CLOSING] Exiting drivers and closing prompt')
+        driver.quit()
+        return True
+ 
+    def do_status(self, inp):
+        if inp == 'online':
+            online()
+        elif inp == 'idle':
+            idle()
+        elif inp == 'dnd':
+            dnd()
+        elif inp == 'offline':
+            offline()
         else:
-            print('Valid 2fa!')
-    else:
+            print(Fore.RED + 'Invalid arguement(s). Accepted arguments: online, idle, dnd, offline, rainbow')
 
-        time.sleep(5)
-        try:
-            driver.find_element_by_xpath('//*[@id="private-channels-0"]/div')
-        except NoSuchElementException:
-            friendsbutton = False
-        friendsbutton = True
+    def help_exit(self):
+       print("Exits the program")
 
-        try:
-            driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/form/div/img')
-        except NoSuchElementException:
-            logo2fa = False
-        logo2fa = True
+    def help_status(self):
+       print("Changes your Discord status | Example usage: status dnd\nAccepted arguments: online, idle, dnd, offline, rainbow")
 
-        if friendsbutton == True & logo2fa == False:
-            print('Successfully Logged in!')
-        else:
-            print('An error occured!')
+cmd = InteractiveCMD()
+cmd.cmdloop()
+print(Fore.GREEN + '[CLOSING] Successfully closed!')
